b0dd324305bc9632fd7ec713a2ca93266ae919bc
rixcx <adts0415.ovo@gmail.com>
2024-12-13 21:48:11 +0900

    [page] formatting API

--- lib/notion/notion.ts
+++ lib/notion/notion.ts
@@ -4,3 +4,34 @@ const { Client } = require("@notionhq/client")
 export const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 })
+
+export const getAllEpisodes = async () => {
+
+  //インスタンス作成
+  const mynotion = notion;
+  
+  //インスタンスをもとにAPIを取得してきて、responceにいれる
+  const response = await mynotion.databases.query({
+    database_id: process.env.DATABASE_ID,
+    sorts: [
+      {
+        property: 'created',
+        direction: 'descending',
+      },
+    ]
+  })
+  
+  //Responseから各記事の必要な情報だけを取り出し配列に入れ直す
+  const episodesProperties = response.results.map((post:any) => {
+    const id = post.id;
+    const number = post.properties.number.number
+    const title = post.properties.title.title[0].plain_text;
+    const date = post.properties.date.date.start;
+    const youtube_url = post.properties.youtube_url.url;
+    const publish = post.properties.publish.checkbox;
+    //slug追加したい
+    return { id,number,title,date,youtube_url,publish };
+  });
+
+  return episodesProperties;
+};
