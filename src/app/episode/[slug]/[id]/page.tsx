b50c3e26df3a6b9f8ce1e22b81a0249ecd29d86b
rixcx <adts0415.ovo@gmail.com>
2024-12-18 18:30:21 +0900

    [tailwindcss] remove taileindcss

--- src/app/episode/[slug]/[id]/page.tsx
+++ src/app/episode/[slug]/[id]/page.tsx
@@ -9,8 +9,8 @@ export default async function EpisodeDetail({ params }: { params: { slug: string
   const detailEpisode = await getDetailEpisodes(id);
   
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="">
+      <main className="">
         <h1>This is【{slug}】page.</h1>
         <p>Page id: {id}</p>
         <ul>
