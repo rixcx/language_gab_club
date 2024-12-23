c36c99cc99b35cceff2648810f1a8d69ab189d43
rixcx <adts0415.ovo@gmail.com>
2024-12-23 23:34:37 +0900

    [google font] set google font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Anton_SC, Roboto } from 'next/font/google'
 
 import Header from '@/src/app/components/header/header';
 import Footer from '@/src/app/components/footer/footer';
@@ -10,15 +10,17 @@ import '@/src/app/styles/global/reset.css'
 import '@/src/app/styles/global/global.scss'
 import css from '@/src/app/styles/Layout.module.scss';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const anton_sc = Anton_SC({
+  weight: "400",
+  variable: '--font-anton_sc',
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+})
+ 
+const roboto = Roboto({
+  weight: ['400', '700'],
+  variable: '--font-roboto',
   subsets: ["latin"],
-});
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -38,7 +40,7 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased ${css.body}`}
+        className={`${anton_sc.variable} ${roboto.variable} antialiased ${css.body}`}
       >
         <div className={css.inner}>
           <Header />
