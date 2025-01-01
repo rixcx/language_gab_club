f7723b5b63cb6ae9c942844c293122069056143f
rixcx <adts0415.ovo@gmail.com>
2025-01-01 21:44:18 +0900

    [css] fix global selector

--- src/app/components/EpisodeSlider.tsx
+++ src/app/components/EpisodeSlider.tsx
@@ -19,44 +19,46 @@ interface EpisodeSliderProps {
 export const EpisodeSlider = ({ episodes }: EpisodeSliderProps) => {
   return (
     <>
-      <Swiper
-        spaceBetween={25}
-        slidesPerView={1.4}
-        modules={[Pagination]}
-        pagination={{
-          type: "bullets",
-          el: ".custom-swiper-pagination",
-          clickable: true,
-        }}
-      >
-        {episodes .map((episode) => (
-          <SwiperSlide key={episode.id}>
-            <div className={styles.episode}>
-              <Link href={`/episodes/${episode.properties.slug.rich_text[0].plain_text}/${episode.id}`}>
-                <div className={styles.episode__img}>
-                  <Image
-                    src={`/episodes/${episode.properties.thumbnail.rich_text[0].plain_text}`}
-                    alt={`${episode.properties.title.title[0].plain_text}`}
-                    width={330}
-                    height={186}
-                  />
-                </div>
-                <div className={styles.episode__inner}>
-                  <div className={styles.episode__detail}>
-                    <span>#{episode.properties.number.number}</span>
-                    <h3>{episode.properties.title.title[0].plain_text}</h3>
+      <div className={styles.swiper_wrap}>
+        <Swiper
+          spaceBetween={25}
+          slidesPerView={1.4}
+          modules={[Pagination]}
+          pagination={{
+            type: "bullets",
+            el: ".custom-swiper-pagination",
+            clickable: true,
+          }}
+        >
+          {episodes .map((episode) => (
+            <SwiperSlide key={episode.id}>
+              <div className={styles.episode}>
+                <Link href={`/episodes/${episode.properties.slug.rich_text[0].plain_text}/${episode.id}`}>
+                  <div className={styles.episode__img}>
+                    <Image
+                      src={`/episodes/${episode.properties.thumbnail.rich_text[0].plain_text}`}
+                      alt={`${episode.properties.title.title[0].plain_text}`}
+                      width={330}
+                      height={186}
+                    />
                   </div>
-                  <div className={styles.episode__bottom}>
-                    <time>{episode.properties.date.date.start}</time>
-                    <div>Listen</div>
+                  <div className={styles.episode__inner}>
+                    <div className={styles.episode__detail}>
+                      <span>#{episode.properties.number.number}</span>
+                      <h3>{episode.properties.title.title[0].plain_text}</h3>
+                    </div>
+                    <div className={styles.episode__bottom}>
+                      <time>{episode.properties.date.date.start}</time>
+                      <div>Listen</div>
+                    </div>
                   </div>
-                </div>
-              </Link>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      <div className="custom-swiper-pagination"></div>
+                </Link>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div className="custom-swiper-pagination"></div>
+      </div>
     </>
   );
 }
